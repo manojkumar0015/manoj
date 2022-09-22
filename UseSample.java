ba33f3ade6d394d0f705502e737b28d788a639f7
manojkumar <manojkumarmanoj10005@gmail.com>
2022-09-22 12:01:53 +0530

    update

--- UseSample.java
+++ UseSample.java
@@ -3,13 +3,13 @@ package classs;
 public class UseSample {
 	public static void main(String[] args) {
 		Sample s1=new Sample();
-		s1.name="manoj";
+		s1.name="anoj";
 		s1.age=54;
 		s1.idCard=56984;
 		s1.isPresent=true;
 		
 		Sample s2=new Sample();
-		s2.name="kabilan";
+		s2.name="abilan";
 		s2.age=25;
 		s2.idCard=47984;
 		s2.isPresent=true;
@@ -23,10 +23,10 @@ public class UseSample {
 		p[0]=s1;
 		p[1]=s2;
 		p[2]=s3;
-		/*for(int i=0;i<p.length;i++) {
+		for(int i=0;i<p.length;i++) {
 			if(p[i].age==25) {
 				System.out.println(p[i].isPresent+p[i].name);
-			}*/
+			}
 		/*int max=0;String name="";
 		for(int i=0;i<p.length;i++) {
 			if(p[i].idCard>max) {
@@ -54,7 +54,8 @@ public class UseSample {
 				
 			}
 			
-		}
+	}
+}
 			
 		 
 		
